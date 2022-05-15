8e97efbedf538764f44d69afc39c717704a40e1a
Sergey Yakimovich <yakimovichsergei@gmail.com>
2022-05-15 18:50:33 +0300

    added test to lesson-4

--- main.js
+++ main.js
@@ -3,23 +3,25 @@ const summary = (x) => (y) => x + y;
 console.log(summary(15)(35));
 
 //Покрасьте абзацы по клику
-const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
+// const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
+//
+// let par1 = document.getElementById('text1');
+// let par2 = document.getElementById('text2');
+// let par3 = document.getElementById('text3');
+//
+// const handleClick = () => {
+//   let i = 0;
+//   return function changeColor(event) {
+//     event.target.style.color = colors[i];
+//     i++;
+//     if (i >= colors.length) {
+//       i = 0;
+//     }
+//   };
+// };
+//
+// par1.addEventListener('click', handleClick());
+// par2.addEventListener('click', handleClick());
+// par3.addEventListener('click', handleClick());
 
-let par1 = document.getElementById('text1');
-let par2 = document.getElementById('text2');
-let par3 = document.getElementById('text3');
-
-const handleClick = () => {
-  let i = 0;
-  return function changeColor(event) {
-    event.target.style.color = colors[i];
-    i++;
-    if (i >= colors.length) {
-      i = 0;
-    }
-  };
-};
-
-par1.addEventListener('click', handleClick());
-par2.addEventListener('click', handleClick());
-par3.addEventListener('click', handleClick());
+module.exports = { summary };
