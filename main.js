315d56f3ead837a5e9a120240ce459f6a0339803
Sergey Yakimovich <yakimovichsergei@gmail.com>
2022-06-24 15:59:32 +0300

    lesson-7 remade

--- main.js
+++ main.js
@@ -1,60 +1,50 @@
 // Напишите функция deepEqual, которая сможет сравнивать 2 объекта с разными уровнями вложенности.
 
 const obj1 = {
+  a: 'a',
+  b: {
     a: 'a',
-    b: {
-        a: 'a',
-        b: 'b',
-        c: {
-            a: 1,
-        },
+    b: 'b',
+    c: {
+      a: 1,
     },
+  },
 };
 const obj2 = {
-    b: {
-        c: {
-            a: 1,
-        },
-        b: 'b',
-        a: 'a',
-    },
-    a: 'a',
-};
-const obj3 = {
-    a: {
-        c: {
-            a: 'a',
-        },
-        b: 'b',
-        a: 'a',
+  b: {
+    c: {
+      a: 1,
     },
     b: 'b',
+    a: 'a',
+  },
+  a: 'a',
+};
+const obj3 = {
+  a: {
+    c: {
+      a: 'a',
+    },
+    b: 'b',
+    a: 'a',
+  },
+  b: 'b',
 };
 
-const deepEqual = (object1, object2) => {
-    for(let prop in object1){
-        if(object1.hasOwnProperty(prop)){
-            if(object1[prop] !== object2[prop]){
-                return false;
-            }
-        }
+const deepEqual = function (object1, object2) {
+  if (
+    typeof object1 === 'object' &&
+    object1 !== null &&
+    typeof object2 === 'object' &&
+    object2 !== null
+  ) {
+    if (Object.keys(object1).length !== Object.keys(object2).length) return false;
+    for (let prop in object1) {
+      if (object2.hasOwnProperty(prop)) return deepEqual(object1[prop], object2[prop]);
     }
-    for(let prop in object2){
-        if(object2.hasOwnProperty(prop)){
-            if(object2[prop] !== object1[prop]){
-                return false;
-            }
-        }
-    }
-    return true;
-}
+  } else if (object1 !== object2) return false;
+  else return true;
+};
 
 console.log(deepEqual(obj1, obj2));
 console.log(deepEqual(obj1, obj3));
-console.log(deepEqual(obj2, obj3));
-
-
-
-
-
-
