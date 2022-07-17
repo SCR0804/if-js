ce2a62fff634f95b4fcc5517b98ed622139fe790
Sergey Yakimovich <yakimovichsergei@gmail.com>
2022-07-17 14:17:10 +0300

    added style for render cards

--- js/main.js
+++ js/main.js
@@ -1,24 +1,21 @@
-
 const parent = document.querySelector('#render_section');
-
 const url = 'https://fe-student-api.herokuapp.com/api/hotels/popular';
 
 async function fetchData() {
-  const response = await fetch(url)
-  const data = await response.json()
-  console.log(data)
-  const cards = data.map(item => `
-    <img src=${item.imageUrl} alt=${item.name}>
+    const response = await fetch(url);
+    const data = await response.json();
+    const cards = data.map(card => `<figure class="homes_pic">
+    <img src=${card.imageUrl} alt=${card.name}>
                     <figcaption class="homes_desc">
-                        ${item.name}
+                        ${card.name}
                     </figcaption>
                     <figcaption class="homes_desc_two">
-                        ${item.city}, ${item.country}
-                    </figcaption>
-  `
-  );
-  parent.insertAdjacentHTML('afterbegin', cards);
+                        ${card.city}, ${card.country}
+                        </figcaption>
+    </figure>`);
+    parent.insertAdjacentHTML('afterbegin', cards);
+};
 
-}
 
-fetchData()
+
+fetchData();
